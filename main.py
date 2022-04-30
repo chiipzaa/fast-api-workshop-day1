74ab6d606eef936ed3bd068e755596d18f25672e
Teerasit.Art <teerasit.art@gmail.com>
2022-04-30 12:34:25 +0700

    fast api success day 1

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from models.database import engine
+from models.inventory import inventory_model
+
+from routers.inventory import inventory_router
+from routers.users import users_router
+
+app = FastAPI()
+app.include_router(inventory_router.router)
+app.include_router(users_router.router)
+
+
+@app.get("/")
+def hello():
+    return {"Hello": "FastAPI"}
+
+
+inventory_model.Base.metadata.create_all(engine)
